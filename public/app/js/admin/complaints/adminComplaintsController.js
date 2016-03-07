112eac9c61956592e3dfadb46ff2361b3402b548
Brack Carmony <brackcarmony@gmail.com>
2016-03-07 15:29:36 -0700

    fixed slide in/out graphic glitch on wide display

--- public/app/js/admin/complaints/adminComplaintsController.js
+++ public/app/js/admin/complaints/adminComplaintsController.js
@@ -3,10 +3,10 @@ var app = angular.module("eloEverything");
 app.controller("adminComplaintsController", function($scope, complaints, complaintsService, questionsService){
   $scope.complaints = complaints;
   $scope.removeComplaint = function(complaint){
-    complaintsService.deleteComplaint(complaint).then(function({
+    complaintsService.deleteComplaint(complaint).then(function(){
       $scope.complaints = _.reject($scope.complaints, function(item){
         return item._id == complaint._id;
-      })
-    }));
+      });
+    });
   }
 });
--- public/app/js/admin/editQuestion/editQuestionController.js
+++ public/app/js/admin/editQuestion/editQuestionController.js
@@ -2,8 +2,8 @@ var app = angular.module("eloEverything")
 
 app.controller("editQuestionController", function($scope, questionsService, $route){
 
-
-  questionsService.getIndividualQuestion($route.params.id).then(function(response){
+  console.log($route.current.params.id);
+  questionsService.getIndividualQuestion($route.current.params.id).then(function(response){
     $scope.question = response;
   });
 
