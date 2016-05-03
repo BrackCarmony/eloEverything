7260ca642e642e84cdfe8ea882d2bf269100b493
Brack Carmony <brackcarmony@gmail.com>
2016-05-02 18:01:56 -0600

    hopefully fix server crash on author stat update

--- server/controllers/statsController.js
+++ server/controllers/statsController.js
@@ -72,11 +72,12 @@ module.exports = {
                 if(!questions || !questions.length) return;
         var allCats = questions.reduce(function(prev, cur){
           cur.scores.reduce(function(prev, cur){
-            if(cur._category && !prev[cur._category._id]){
-              prev[cur._category.status]++;
+            if ( cur._category){
+              if(!prev[cur._category._id]){
+                prev[cur._category.status]++;
+              }
+              prev[cur._category._id]=true
             }
-            prev[cur._category._id]=true
-
             return prev;
           }, prev);
           prev.timesQuestionsAsked += cur.answered;
