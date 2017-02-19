0389127f5b06fe425c14773ced4a2e483eefa586
Kedirech <brackcarmony@gmail.com>
2017-02-19 08:35:42 -0600

    Remove unneded code

--- public/app/js/auth/authController.js
+++ public/app/js/auth/authController.js
@@ -11,15 +11,4 @@ app.controller('authController', function($scope, authService ,$location){
     //console.log("redirect to " + url);
     $location.path(url);
   }
-
-  $scope.scores = [
-    {name:'questions', scores:[1,2,3,4,5,6]},
-    {name:'players', scores:[]}
-  ]
-
-  for (var i=0;i<100;i++){
-    $scope.scores[0].scores.push(Math.random()*100);
-    $scope.scores[1].scores.push(Math.random()*50+100);
-  }
-
 });
